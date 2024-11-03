32cf19989c36b92b7687eac49852c1d06a6cf742
XsporosX@gmail.com <saul_751@hotmail.com>
2024-11-03 02:39:51 -0400

    otras modificaciones

--- src/modules/stripe/payment.service.ts
+++ src/modules/stripe/payment.service.ts
@@ -47,7 +47,7 @@ export class StripeService {
   private stripe: Stripe;
 
   constructor() {
-    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
       apiVersion: '2024-09-30.acacia',
     });
   }
